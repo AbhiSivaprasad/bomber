f7d6cd3aec719bf074291aa23faf6956c15d5305
AbhiSivaprasad <abhi@live.com>
2020-03-22 10:56:44 -0700

    map static directory in fs to /static route

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let route = warp::get()
+    // /GET / serves index.html
+    let index_route = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file("./static/index.html"));
 
-    warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
+    // /GET /static/* serves from ./static dir
+    let static_res_route = warp::path("static").and(warp::fs::dir("./static/"));
+
+    let routes = index_route.or(static_res_route);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
