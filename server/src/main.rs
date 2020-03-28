fd466d89da040be814032c0bf2716c77986f9097
AbhiSivaprasad <abhi@live.com>
2020-03-28 12:35:04 -0700

    wired new paths to client dir

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,10 +5,10 @@ async fn main() {
     // /GET / serves index.html
     let index_route = warp::get()
         .and(warp::path::end())
-        .and(warp::fs::file("./static/index.html"));
+        .and(warp::fs::file("../client/dist/index.html"));
 
-    // /GET /static/* serves from ./static dir
-    let static_res_route = warp::path("static").and(warp::fs::dir("./static/"));
+    // /GET /* serves from ../client/dist dir
+    let static_res_route = warp::fs::dir("../client/dist");
 
     let routes = index_route.or(static_res_route);
 
